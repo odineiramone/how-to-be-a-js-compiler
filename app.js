8892278e0ce54aa00bb535f8027ef1e520bec590
Odinei Rocha <odinei.ribeiro@live.com>
2017-09-09 11:07:11 -0300

    generator

--- app.js
+++ app.js
@@ -90,3 +90,19 @@ function transformer (ast) {
 
   return svg_ast
 }
+
+function generator (svg_ast) {
+  function createAttrString (attr) {
+    return Object.keys(attr).map(function (key) {
+      return key + '= "' + attr[key] + '"'
+    }).join(' ')
+  }
+
+  var svg_attr = createAttrString(svg_ast.attr)
+
+  var elements = svg_ast.body.map(function (node) {
+    return '<' + node.tag + ' ' + createAttrString(node.attr) + '></' + node.tag + '>'
+  }).join('\n\t')
+
+  return '<svg ' + svg_attr + '>\n' + elements + '\n</svg>'
+}
