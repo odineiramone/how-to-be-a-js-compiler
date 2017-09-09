f9de25989ad5759784058b3158c1d5e3b50ccdfd
Odinei Rocha <odinei.ribeiro@live.com>
2017-09-09 11:10:39 -0300

    split code in multiple files

---
+++ parser.js
@@ -0,0 +1,40 @@
+function parser (tokens) {
+  var AST = {
+    type: 'Drawing',
+    body: []
+  }
+
+  while (tokens.length > 0) {
+    var current_token = tokens.shift()
+    if (current_token.type === 'word') {
+      switch (current_token.value) {
+        case 'Paper':
+          var expression = {
+            type: 'CallExpression',
+            name: 'Paper',
+            arguments: []
+          }
+
+          var argument = tokens.shift()
+
+          if (argument.type === 'number') {
+            expression.arguments.push({
+              type:  'NumberLiteral',
+              value:  argument.value
+            })
+            AST.body.push(expression)
+          } else {
+            throw 'Papper command must be followed by a number'
+          }
+          break
+          case 'Pen' :
+
+          case 'Line' :
+        default:
+
+      }
+    }
+  }
+
+  return AST
+}
---
+++ transformer.js
@@ -0,0 +1,39 @@
+function transformer (ast) {
+  var svg_ast = {
+    tag : 'svg',
+    attr: {
+      width: 100, height: 100, viewBox: '0 0 100 100',
+      xmlns: 'http://www.w3.org/2000/svg', version: '1.1'
+    },
+    body: []
+  }
+
+  var pen_color = 100
+
+  while (ast.body.length > 0) {
+    var node = ast.body.shift()
+    switch (node.name) {
+      case 'Paper':
+        var paper_color = 100 - node.arguments[0].value
+        svg_ast.body.push({
+          tag : 'rect',
+          attr: {
+            x: 0, y: 0,
+            width: 100, height: 100,
+            fill: 'rgb(' + paper_color + '%, ' + paper_color + '%, ' + paper_color + '%)'
+          }
+        })
+        break
+      case 'Pen':
+        pen_color = 100 - node.arguments[0].value
+        break
+
+      case 'Line':
+        break
+      default:
+
+    }
+  }
+
+  return svg_ast
+}
